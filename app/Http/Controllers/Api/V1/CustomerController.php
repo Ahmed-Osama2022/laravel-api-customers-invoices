144b58d249634afbc9e6abdf0c9095340a5d4450
Ahmed-Osama2022 <ahmadosama.2011@gmail.com>
2025-06-24 15:36:35 +0300

    Fix loosing the query params on the on the pagination. for Both Customers and Invoices.

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -27,7 +27,10 @@ class CustomerController extends Controller
     if (count($queryItems) == 0) {
       return new CustomerCollection(Customer::paginate());
     } else {
-      return new CustomerCollection(Customer::where($queryItems)->paginate());
+      // return new CustomerCollection(Customer::where($queryItems)->paginate());
+      $invoices = Customer::where($queryItems)->paginate();
+
+      return new CustomerCollection($invoices->appends($request->query()));
     }
 
 
