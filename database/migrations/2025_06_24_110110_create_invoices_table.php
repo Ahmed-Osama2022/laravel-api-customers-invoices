6b61c53a2432c3c957fd94c6e6a695fb5dc49dee
Ahmed-Osama2022 <ahmadosama.2011@gmail.com>
2025-06-24 11:09:22 +0300

    Finish teh migration.

--- database/migrations/2025_06_24_110110_create_invoices_table.php
+++ database/migrations/2025_06_24_110110_create_invoices_table.php
@@ -1,27 +1,34 @@
 <?php
 
+use App\Models\Customer;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('invoices', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('invoices', function (Blueprint $table) {
+      $table->id();
+      $table->foreignIdFor(Customer::class)->constrained()->cascadeOnDelete();
+      $table->integer('amount');
+      $table->string('status'); // Billed, Paid, Void
+      $table->dateTime('billed_date');
+      $table->dateTime('paid_date')->nullable();
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('invoices');
-    }
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('invoices');
+  }
 };
