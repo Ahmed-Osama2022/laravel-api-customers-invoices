760ed799c3c3252ec5d42fcea8c1695d1e8314ba
Ahmed-Osama2022 <ahmadosama.2011@gmail.com>
2025-06-24 11:21:22 +0300

    Build the factories.

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -9,15 +9,25 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class CustomerFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
-        return [
-            //
-        ];
-    }
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    $type = $this->faker->randomElement(['I', 'B']); // (I) for Individual || (B) for Bussnies
+    $name = $type == 'I' ? $this->faker->name() : $this->faker->company();
+
+    return [
+      'name' => $name,
+      'type' => $type,
+      // 'email' => fake()->unique()->safeEmail(),
+      'email' => $this->faker->email(),
+      'state' => $this->faker->state(),
+      'city' => $this->faker->city(),
+      'address' => $this->faker->address(),
+      'postal_code' => $this->faker->postcode(),
+    ];
+  }
 }
--- database/factories/InvoiceFactory.php
+++ database/factories/InvoiceFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Customer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,15 +10,23 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class InvoiceFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
-        return [
-            //
-        ];
-    }
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    // status =>  Billed, Paid, Void
+    $status = $this->faker->randomElement(['B', 'P', 'V']);
+
+    return [
+      'customer_id' => Customer::factory(),
+      'amount' => $this->faker->numberBetween(100, 2000),
+      'status' => $status,
+      // 'billed_date' => $this->faker->dateTimeBetween('-1 year', 'now'),
+      'billed_date' => $this->faker->dateTimeThisDecade(),
+      'paid_date' => $status == 'P' ? $this->faker->dateTimeThisDecade() : null
+    ];
+  }
 }
