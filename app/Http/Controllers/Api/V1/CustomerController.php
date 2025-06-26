e8c39f134fb381777c65ea834ecaf71ef2d8fbb7
Ahmed-Osama2022 <ahmadosama.2011@gmail.com>
2025-06-26 11:48:04 +0300

    A little change to fit with the API versioning; and prepare fot store method.

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Filters\V1\CustomerFilter;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreCustomerRequest;
-use App\Http\Requests\UpdateCustomerRequest;
+use App\Http\Requests\V1\StoreCustomerRequest;
+use App\Http\Requests\V1\UpdateCustomerRequest;
 use App\Http\Resources\V1\CustomerCollection;
 use App\Http\Resources\V1\CustomerResource;
 use App\Models\Customer;
@@ -62,7 +62,8 @@ class CustomerController extends Controller
    */
   public function store(StoreCustomerRequest $request)
   {
-    //
+    // NOTE: return the new entity
+    return new CustomerResource(Customer::create([$request->all()]));
   }
 
   /**
--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -10,7 +10,7 @@ class Customer extends Model
 {
   use HasFactory;
 
-  // protected $fillable = ['name', 'email', 'phone', 'address', 'city', 'state', 'post'];
+  protected $fillable = ['name', 'email', 'address', 'city', 'state', 'postalCode'];
 
 
   public function invoices(): HasMany
