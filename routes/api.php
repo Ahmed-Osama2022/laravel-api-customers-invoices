ba66aeea6ec9c44b2a96cdcb484bbaf3139c5066
Ahmed-Osama2022 <ahmadosama.2011@gmail.com>
2025-06-27 11:21:05 +0300

    Start working with  Authentication using (sanctum)

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,13 @@
 
 use App\Http\Controllers\Api\V1\CustomerController;
 use App\Http\Controllers\Api\V1\InvoiceController;
+use App\Models\Customer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+// Auth:
+// use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,9 +29,31 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 // Route::prefix('v1')->group(function () {});
 // Route::prefix('v1')->namespace('App\Http\Controllers\Api\V1')->group(function () {});
 
-Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1', 'middleware' => 'auth:sanctum'], function () {
   Route::apiResource('customers', CustomerController::class);
   Route::apiResource('invoices', InvoiceController::class);
 
   Route::post('invoices/bulk', [InvoiceController::class, 'bulkStore']);
+});
+
+
+/**
+ * For the Authentication
+ */
+Route::post('/sanctum/token', function (Request $request) {
+  $request->validate([
+    'email' => 'required|email',
+    // 'password' => 'required',
+    // 'device_name' => 'required',
+  ]);
+
+  $user = Customer::where('email', $request->email)->first();
+
+  if (! $user) {
+    throw ValidationException::withMessages([
+      'email' => ['The provided credentials are incorrect.'],
+    ]);
+  }
+
+  return $user->createToken($request->email)->plainTextToken;
 });
